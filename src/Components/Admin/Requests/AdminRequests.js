873b9354850dfc399c0a899d38b5eabeb0bbb591
brinnathan <82681242+brinnathan@users.noreply.github.com>
2024-04-22 19:06:29 -0700

    Add files via upload

--- src/Components/Admin/Requests/AdminRequests.js
+++ src/Components/Admin/Requests/AdminRequests.js
@@ -1,208 +1,231 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from "react";
 import API from "../../../API/API_Interface";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Popover from '@mui/material/Popover';
-import IconButton from '@mui/material/IconButton';
-import SettingsIcon from '@mui/icons-material/Settings';
-import MenuItem from '@mui/material/MenuItem';
-import MenuList from '@mui/material/MenuList';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import CheckIcon from '@mui/icons-material/Check';
-import ClearIcon from '@mui/icons-material/Clear';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Popover from "@mui/material/Popover";
+import IconButton from "@mui/material/IconButton";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
 import { FcSettings } from "react-icons/fc";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 
 const requestsTableAttributes = [
-    {
-        title: 'Name',
-        attributeDBName: 'name',
-        align: 'left'
-    },
-    {
-        title: 'Start',
-        attributeDBName: 'start_time',
-        align: 'left'
-    },
-    {
-        title: 'End',
-        attributeDBName: 'end_time',
-        align: 'left'
-    },
-    {
-        title: 'Reason',
-        attributeDBName: 'reason',
-        align: 'left'
-    },
-    {
-        title: 'Status',
-        attributeDBName: 'status',
-        align: 'left'
-    }
+  {
+    title: "Name",
+    attributeDBName: "name",
+    align: "left",
+  },
+  {
+    title: "Start",
+    attributeDBName: "start_time",
+    align: "left",
+  },
+  {
+    title: "End",
+    attributeDBName: "end_time",
+    align: "left",
+  },
+  {
+    title: "Reason",
+    attributeDBName: "reason",
+    align: "left",
+  },
+  {
+    title: "Status",
+    attributeDBName: "status",
+    align: "left",
+  },
 ];
 
 const RequestTable = () => {
-    const [requests, setRequests] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [anchorEl, setAnchorEl] = useState(null);
+  const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    useEffect(() => {
-        const fetchRequests = async () => {
-            try {
-                const api = new API();
-                const requestsJSONString = await api.allRequests();
-                setRequests(requestsJSONString.data);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-                setLoading(false);
-            }
-        };
-
-        fetchRequests();
-    }, []);
-
-    const handleOpenPopover = (event) => {
-        setAnchorEl(event.currentTarget);
+  useEffect(() => {
+    const fetchRequests = async () => {
+      try {
+        const api = new API();
+        const requestsJSONString = await api.allRequests();
+        setRequests(requestsJSONString.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
     };
 
-    const handleClosePopover = () => {
-        setAnchorEl(null);
-    };
+    fetchRequests();
+  }, []);
 
-    const handleApprove = () => {
-        const confirmed = window.confirm("Are you sure you want to approve this request?");
-        if (confirmed) {
-            // Perform the approve action
-            handleClosePopover();
-        }
-    };
+  const handleOpenPopover = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleDeny = () => {
-        const confirmed = window.confirm("Are you sure you want to deny this request??");
-        if (confirmed) {
-            // Perform the approve action
-            handleClosePopover();
-        }
-    };
+  const handleClosePopover = () => {
+    setAnchorEl(null);
+  };
 
-    const open = Boolean(anchorEl);
+  const handleApprove = () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to approve this request?"
+    );
+    if (confirmed) {
+      // Perform the approve action
+      handleClosePopover();
+    }
+  };
 
-    // Function to format date in desired format
-    const formatDate = (dateString) => {
-        const options = { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' };
-        return new Date(dateString).toLocaleDateString('en-US', options);
-    };
+  const handleDeny = () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to deny this request??"
+    );
+    if (confirmed) {
+      // Perform the approve action
+      handleClosePopover();
+    }
+  };
 
-    const getStatusColor = (status) => {
-        switch (status) {
-            case 'Approved':
-                return 'green';
-            case 'Pending':
-                return 'orange';
-            case 'Denied':
-                return 'red';
-            default:
-                return 'inherit';
-        }
-    };
-    
-    const getStatusText = (status) => {
-        switch (status) {
-            case 'Approved':
-                return 'Approved';
-            case 'Pending':
-                return 'Pending';
-            case 'Denied':
-                return 'Denied';
-            default:
-                return 'Unknown';
-        }
-    };
+  const open = Boolean(anchorEl);
 
-    const renderTableRow = (requestObject, index) => (
-        <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            {requestsTableAttributes.map((attr, idx) => (
+  // Function to format date in desired format
+  const formatDate = (dateString) => {
+    const options = {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    };
+    return new Date(dateString).toLocaleDateString("en-US", options);
+  };
+
+  const getStatusColor = (status) => {
+    switch (status) {
+      case "Approved":
+        return "green";
+      case "Pending":
+        return "orange";
+      case "Denied":
+        return "red";
+      default:
+        return "inherit";
+    }
+  };
+
+  const getStatusText = (status) => {
+    switch (status) {
+      case "Approved":
+        return "Approved";
+      case "Pending":
+        return "Pending";
+      case "Denied":
+        return "Denied";
+      default:
+        return "Unknown";
+    }
+  };
+
+  const renderTableRow = (requestObject, index) => (
+    <TableRow
+      key={index}
+      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    >
+      {requestsTableAttributes.map((attr, idx) => (
+        <TableCell key={idx} align={attr.align}>
+          {attr.attributeDBName === "status" ? (
+            <Typography
+              variant="body1"
+              style={{
+                color: getStatusColor(requestObject[attr.attributeDBName]),
+                fontSize: "0.9rem",
+              }}
+            >
+              {getStatusText(requestObject[attr.attributeDBName])}
+            </Typography>
+          ) : attr.attributeDBName && attr.attributeDBName.includes("time") ? (
+            formatDate(requestObject[attr.attributeDBName])
+          ) : (
+            requestObject[attr.attributeDBName]
+          )}
+        </TableCell>
+      ))}
+      <TableCell align="right">
+        {requestObject.status === "Pending" && (
+          <IconButton onClick={handleOpenPopover}>
+            <FcSettings />
+          </IconButton>
+        )}
+        <Popover
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClosePopover}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          PaperProps={{
+            sx: {
+              boxShadow: "1px 1px 3px rgba(0, 0, 0, 0.2)", // Customize the boxShadow property
+            },
+          }}
+        >
+          <MenuList>
+            <MenuItem onClick={handleApprove}>
+              <ListItemIcon sx={{ color: "green" }}>
+                <CheckIcon />
+              </ListItemIcon>
+              Approve
+            </MenuItem>
+            <MenuItem onClick={handleDeny}>
+              <ListItemIcon sx={{ color: "red" }}>
+                <ClearIcon />
+              </ListItemIcon>
+              Deny
+            </MenuItem>
+          </MenuList>
+        </Popover>
+      </TableCell>
+    </TableRow>
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <Fragment>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="requests table">
+          <TableHead>
+            <TableRow>
+              {requestsTableAttributes.map((attr, idx) => (
                 <TableCell key={idx} align={attr.align}>
-                    {attr.attributeDBName === 'status' ? (
-                    <Typography variant="body1" style={{ color: getStatusColor(requestObject[attr.attributeDBName]), fontSize: '0.9rem' }}>
-                        {getStatusText(requestObject[attr.attributeDBName])}
-                    </Typography>
-                ) : (
-                    attr.attributeDBName && attr.attributeDBName.includes('time') 
-                        ? formatDate(requestObject[attr.attributeDBName]) 
-                        : requestObject[attr.attributeDBName]
-                )}
+                  {attr.title}
                 </TableCell>
-            ))}
-            <TableCell align="right">
-            {requestObject.status === 'Pending' && (
-                <IconButton onClick={handleOpenPopover}>
-                    <FcSettings />
-                </IconButton>
-                 )}
-                <Popover
-                    open={open}
-                    anchorEl={anchorEl}
-                    onClose={handleClosePopover}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                >
-                    <MenuList>
-                        <MenuItem onClick={handleApprove}>
-                            <ListItemIcon sx={{ color: 'green' }}>
-                                <CheckIcon />
-                            </ListItemIcon>
-                            Approve
-                        </MenuItem>
-                        <MenuItem onClick={handleDeny}>
-                            <ListItemIcon sx={{ color: 'red' }}>
-                                <ClearIcon />
-                            </ListItemIcon>
-                            Deny
-                        </MenuItem>
-                    </MenuList>
-                </Popover>
-            </TableCell>
-        </TableRow>
-    );
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
-
-    return (
-        <Fragment>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="requests table">
-                    <TableHead>
-                        <TableRow>
-                            {requestsTableAttributes.map((attr, idx) => (
-                                <TableCell key={idx} align={attr.align}>
-                                    {attr.title}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {requests.map((request, idx) => renderTableRow(request, idx))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Fragment>
-    );
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {requests.map((request, idx) => renderTableRow(request, idx))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Fragment>
+  );
 };
 
 export default RequestTable;
