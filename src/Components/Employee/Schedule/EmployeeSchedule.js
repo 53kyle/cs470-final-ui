369f37833facbbe9667b1be90e75ec90e1caf10b
brinnathan <82681242+brinnathan@users.noreply.github.com>
2024-04-23 20:07:19 -0700

    Add files via upload

--- src/Components/Employee/Schedule/EmployeeSchedule.js
+++ src/Components/Employee/Schedule/EmployeeSchedule.js
@@ -1,168 +1,253 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, { useState, useEffect, Fragment } from "react";
 import API from "../../../API/API_Interface";
 
 import DateHelper from "../../../Utils/DateHelper";
 import ScheduleHelper from "../../../Utils/ScheduleHelper";
 import SampleData from "../../../Utils/SampleData";
 import ScheduleTopBar from "../../Generic/ScheduleTopBar";
-import {Box, Divider, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
+import {
+  Box,
+  Divider,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
-function EmployeeScheduleTable({user, currentWeek}) {
-    const [shifts, setShifts] = useState([]);
+function EmployeeScheduleTable({ user, currentWeek }) {
+  const [shifts, setShifts] = useState([]);
+  const theme = useTheme();
 
-    useEffect(() => {
-        async function fetchData() {
 
-            try {
-                const api = new API();
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const api = new API();
 
-                const shiftsResponse = await api.shiftsForEmployeeInRange(DateHelper.dateToMySQLDate(currentWeek[0]), DateHelper.dateToMySQLDate(currentWeek[6]), user.employee_id);
-                setShifts(shiftsResponse.data);
+        const shiftsResponse = await api.shiftsForEmployeeInRange(
+          DateHelper.dateToMySQLDate(currentWeek[0]),
+          DateHelper.dateToMySQLDate(currentWeek[6]),
+          user.employee_id
+        );
+        setShifts(shiftsResponse.data);
 
-                console.log("Shifts: " + JSON.stringify(shiftsResponse.data))
-                console.log(new Date((shiftsResponse.data[0].date)))
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        }
+        console.log("Shifts: " + JSON.stringify(shiftsResponse.data));
+        console.log(new Date(shiftsResponse.data[0].date));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
 
-        fetchData();
-    }, [currentWeek, user]);
+    fetchData();
+  }, [currentWeek, user]);
 
-    return (
-        <Paper sx={{ width: '100%', overflow: 'hidden', minWidth: 610 }}>
-            <TableContainer sx={{ flexGrow: 1 }}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {
-                                currentWeek.map((date, idx) => (
-                                    <TableCell
-                                        key={idx}
-                                        align="center"
-                                        style={{ minWidth: 80, maxWidth: 80, borderLeft: "1px solid rgba(224, 224, 224, 1)", backgroundColor: "#eff4fb" }}
-                                    >
-                                        <Fragment>
-                                            <Typography variant="caption" align="center" component="div">
-                                                {
-                                                    DateHelper.getPlainWeekday(idx)
-                                                }
-                                            </Typography>
-                                            <Typography variant="subtitle1" align="center" component="div">
-                                                {
-                                                    DateHelper.shorterDateFormat(date)
-                                                }
-                                            </Typography>
-                                        </Fragment>
-                                    </TableCell>
-                                ))
-                            }
-                            <TableCell
-                                key={8}
-                                align="center"
-                                style={{ minWidth: 50, maxWidth: 50, borderLeft: "1px solid rgba(224, 224, 224, 1)", backgroundColor: "#eff4fb" }}
-                            >
-                                <Typography variant="subtitle1" align="center" component="div">
-                                    Total Hours
-                                </Typography>
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow tabIndex={-1} key={1}>
-                            {
-                                currentWeek.map((date, idx) => (
-                                    <TableCell
-                                        key={idx}
-                                        align="center"
-                                        style={{ minWidth: 80, maxWidth: 80, borderLeft: "1px solid rgba(224, 224, 224, 1)" }}
-                                    >
-                                        {
-                                            shifts.filter(s => DateHelper.dateToMySQLDate(DateHelper.textToDate(s.date)) == DateHelper.dateToMySQLDate(date)).length > 0 ?
-                                            <Box sx={{
-                                                display: "flex",
-                                                flexDirection: "column",
-                                                alignItems: "center",
-                                                justifyContent: "center"
-                                            }}>
-                                                <Typography variant="subtitle1" align="center" component="div">
-                                                    {
-                                                        ScheduleHelper.getTimesForShift(shifts.filter(s => DateHelper.dateToMySQLDate(DateHelper.textToDate(s.date)) == DateHelper.dateToMySQLDate(date))[0])
-                                                    }
-                                                </Typography>
-                                                <Typography variant="caption" align="center" component="div">
-                                                    {
-                                                        `Meal: ${ScheduleHelper.getMealTimesForShift(shifts.filter(s => DateHelper.dateToMySQLDate(DateHelper.textToDate(s.date)) == DateHelper.dateToMySQLDate(date))[0])}`
-                                                    }
-                                                </Typography>
-                                                <Typography variant="caption" align="center" component="div">
-                                                    {
-                                                        shifts.filter(s => DateHelper.dateToMySQLDate(DateHelper.textToDate(s.date)) == DateHelper.dateToMySQLDate(date))[0].department
-                                                    }
-                                                </Typography>
-                                                <Typography variant="caption" align="center" component="div">
-                                                    {
-                                                        `${ScheduleHelper.getHoursForShift(shifts.filter(s => DateHelper.dateToMySQLDate(DateHelper.textToDate(s.date)) == DateHelper.dateToMySQLDate(date))[0])} hours`
-                                                    }
-                                                </Typography>
-                                            </Box> :
-                                                <Typography variant="subtitle1" align="center" component="div" color="gray">
-                                                    OFF
-                                                </Typography>
-                                        }
-                                    </TableCell>
-                                ))
-                            }
-                            <TableCell
-                                key={8}
-                                align="center"
-                                style={{ minWidth: 50, maxWidth: 50, borderLeft: "1px solid rgba(224, 224, 224, 1)" }}
-                            >
-                                <Typography variant="subtitle1" align="center" component="div">
-                                    {
-                                        ScheduleHelper.getHoursForSchedule(shifts)
-                                    }
-                                </Typography>
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Paper>
-    );
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden", minWidth: 610 }}>
+      <TableContainer sx={{ flexGrow: 1 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {currentWeek.map((date, idx) => (
+                <TableCell
+                  key={idx}
+                  align="center"
+                  style={{
+                    minWidth: 80,
+                    maxWidth: 80,
+                    borderLeft: "1px solid rgba(224, 224, 224, 1)",
+                    backgroundColor: theme.palette.primary.main,
+                    color: "white"
+                  }}
+                >
+                  <Fragment>
+                    <Typography
+                      variant="caption"
+                      align="center"
+                      component="div"
+                    >
+                      {DateHelper.getPlainWeekday(idx)}
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      align="center"
+                      component="div"
+                    >
+                      {DateHelper.shorterDateFormat(date)}
+                    </Typography>
+                  </Fragment>
+                </TableCell>
+              ))}
+              <TableCell
+                key={8}
+                align="center"
+                style={{
+                  minWidth: 50,
+                  maxWidth: 50,
+                  borderLeft: "1px solid rgba(224, 224, 224, 1)",
+                  backgroundColor: theme.palette.primary.main,
+                  color: "white"
+                }}
+              >
+                <Typography variant="subtitle1" align="center" component="div">
+                  Total Hours
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow tabIndex={-1} key={1}>
+              {currentWeek.map((date, idx) => (
+                <TableCell
+                  key={idx}
+                  align="center"
+                  style={{
+                    minWidth: 80,
+                    maxWidth: 80,
+                    borderLeft: "1px solid rgba(224, 224, 224, 1)",
+                  }}
+                >
+                  {shifts.filter(
+                    (s) =>
+                      DateHelper.dateToMySQLDate(
+                        DateHelper.textToDate(s.date)
+                      ) == DateHelper.dateToMySQLDate(date)
+                  ).length > 0 ? (
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Typography
+                        variant="subtitle1"
+                        align="center"
+                        component="div"
+                      >
+                        {ScheduleHelper.getTimesForShift(
+                          shifts.filter(
+                            (s) =>
+                              DateHelper.dateToMySQLDate(
+                                DateHelper.textToDate(s.date)
+                              ) == DateHelper.dateToMySQLDate(date)
+                          )[0]
+                        )}
+                      </Typography>
+                      <Typography
+                        variant="caption"
+                        align="center"
+                        component="div"
+                      >
+                        {`Meal: ${ScheduleHelper.getMealTimesForShift(
+                          shifts.filter(
+                            (s) =>
+                              DateHelper.dateToMySQLDate(
+                                DateHelper.textToDate(s.date)
+                              ) == DateHelper.dateToMySQLDate(date)
+                          )[0]
+                        )}`}
+                      </Typography>
+                      <Typography
+                        variant="caption"
+                        align="center"
+                        component="div"
+                      >
+                        {
+                          shifts.filter(
+                            (s) =>
+                              DateHelper.dateToMySQLDate(
+                                DateHelper.textToDate(s.date)
+                              ) == DateHelper.dateToMySQLDate(date)
+                          )[0].department
+                        }
+                      </Typography>
+                      <Typography
+                        variant="caption"
+                        align="center"
+                        component="div"
+                      >
+                        {`${ScheduleHelper.getHoursForShift(
+                          shifts.filter(
+                            (s) =>
+                              DateHelper.dateToMySQLDate(
+                                DateHelper.textToDate(s.date)
+                              ) == DateHelper.dateToMySQLDate(date)
+                          )[0]
+                        )} hours`}
+                      </Typography>
+                    </Box>
+                  ) : (
+                    <Typography
+                      variant="subtitle1"
+                      align="center"
+                      component="div"
+                      color="gray"
+                    >
+                      OFF
+                    </Typography>
+                  )}
+                </TableCell>
+              ))}
+              <TableCell
+                key={8}
+                align="center"
+                style={{
+                  minWidth: 50,
+                  maxWidth: 50,
+                  borderLeft: "1px solid rgba(224, 224, 224, 1)",
+                }}
+              >
+                <Typography variant="subtitle1" align="center" component="div">
+                  {ScheduleHelper.getHoursForSchedule(shifts)}
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 }
 
-function EmployeeSchedule({user}) {
-    const [currentDate, setCurrentDate] = useState(Date.now());
-    const [currentWeek, setCurrentWeek] = useState(DateHelper.weekOf(Date.now()));
+function EmployeeSchedule({ user }) {
+  const [currentDate, setCurrentDate] = useState(Date.now());
+  const [currentWeek, setCurrentWeek] = useState(DateHelper.weekOf(Date.now()));
 
-    const setPrevWeek = () => {
-        const newDate = currentDate - DateHelper.millisecondsInDay*7;
-        setCurrentDate(newDate);
-        setCurrentWeek(DateHelper.weekOf(newDate));
-    }
+  const setPrevWeek = () => {
+    const newDate = currentDate - DateHelper.millisecondsInDay * 7;
+    setCurrentDate(newDate);
+    setCurrentWeek(DateHelper.weekOf(newDate));
+  };
 
-    const setNextWeek = () => {
-        const newDate = currentDate + DateHelper.millisecondsInDay*7;
-        setCurrentDate(newDate);
-        setCurrentWeek(DateHelper.weekOf(newDate));
-    }
+  const setNextWeek = () => {
+    const newDate = currentDate + DateHelper.millisecondsInDay * 7;
+    setCurrentDate(newDate);
+    setCurrentWeek(DateHelper.weekOf(newDate));
+  };
 
-    return (
-        <Fragment>
-            <ScheduleTopBar
-                datesLabel={DateHelper.dateRangeFormat(currentWeek[0], currentWeek[6])}
-                setPrevWeek={() => setPrevWeek()}
-                setNextWeek={() => setNextWeek()}
-            />
+  return (
+    <Fragment>
+      <ScheduleTopBar
+        datesLabel={DateHelper.dateRangeFormat(currentWeek[0], currentWeek[6])}
+        setPrevWeek={() => setPrevWeek()}
+        setNextWeek={() => setNextWeek()}
+      />
 
-            <Divider sx={{
-                mt: 3
-            }}/>
+      <Divider
+        sx={{
+          mt: 3,
+        }}
+      />
 
-            <EmployeeScheduleTable user={user} currentWeek={currentWeek}/>
-        </Fragment>
-    )
+      <EmployeeScheduleTable user={user} currentWeek={currentWeek} />
+    </Fragment>
+  );
 }
 
 export default EmployeeSchedule;
