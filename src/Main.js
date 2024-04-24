69eaa62ffe1707cb1c6d2b009f9e454922f305aa
brinnathan <82681242+brinnathan@users.noreply.github.com>
2024-04-23 19:56:56 -0700

    Add files via upload

--- src/Main.js
+++ src/Main.js
@@ -1,34 +1,29 @@
-import React, { useState, Fragment } from 'react';
-import Login from './Components/Login/Login';
-import App from './App';
+import React, { useState, Fragment } from "react";
+import Login from "./Components/Login/Login";
+import App from "./App";
+import { lightTheme } from "./Utils/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const logout = (setUser) => {
-
-    return () => {
-        setUser(undefined);
-    }
+  return () => {
+    setUser(undefined);
+  };
 };
 
 function Main() {
-    const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState(undefined);
 
-    return (
-        <Fragment>
-            {
-                user !== undefined ? (
-                    <App
-                        user={user}
-                        logoutAction={logout(setUser)}
-                    />
-                ) : (
-                    <Login
-                        user={user}
-                        setUser={setUser}
-                    />
-                )
-            }
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <ThemeProvider theme={lightTheme}>
+        {user !== undefined ? (
+          <App user={user} logoutAction={logout(setUser)} />
+        ) : (
+          <Login user={user} setUser={setUser} />
+        )}
+      </ThemeProvider>
+    </Fragment>
+  );
 }
 
 export default Main;
