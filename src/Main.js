63d0b30f911b6a74d8311cb74567ba01c1df4bf9
brinnathan <82681242+brinnathan@users.noreply.github.com>
2024-04-23 19:55:55 -0700

    Add files via upload

--- src/Main.js
+++ src/Main.js
@@ -1,55 +1,29 @@
-import React, { useState, Fragment } from 'react';
-import Login from './Components/Login/Login';
-import App from './App';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from "@mui/material/CssBaseline";
-
-const theme = createTheme({
-    palette: {
-        primary:  {
-          main: '#428bca',
-          contrastText: "#fff"  // Change to your primary color
-        },
-        secondary: {
-          main: '#CFB095', // Change to your secondary color
-        },
-      },
-      typography: {
-        fontFamily: '', // Change to your preferred font family
-        fontSize: 16, // Change to your preferred base font size
-      },
-  });
+import React, { useState, Fragment } from "react";
+import Login from "./Components/Login/Login";
+import App from "./App";
+import { lightTheme } from "./Utils/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const logout = (setUser) => {
-
-    return () => {
-        setUser(undefined);
-    }
+  return () => {
+    setUser(undefined);
+  };
 };
 
 function Main() {
-    const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState(undefined);
 
-    return (
-        <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Fragment>
-            {
-                user !== undefined ? (
-                    <App
-                        user={user}
-                        logoutAction={logout(setUser)}
-                    />
-                ) : (
-                    <Login
-                        user={user}
-                        setUser={setUser}
-                    />
-                )
-            }
-        </Fragment>
-        </ThemeProvider>
-    )
+  return (
+    <Fragment>
+      <ThemeProvider theme={lightTheme}>
+        {user !== undefined ? (
+          <App user={user} logoutAction={logout(setUser)} />
+        ) : (
+          <Login user={user} setUser={setUser} />
+        )}
+      </ThemeProvider>
+    </Fragment>
+  );
 }
 
 export default Main;
