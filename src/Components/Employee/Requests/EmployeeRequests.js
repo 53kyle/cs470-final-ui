888721d8e925816f2d58fc5a84ca971e754f0f06
brinnathan <82681242+brinnathan@users.noreply.github.com>
2024-04-29 21:49:47 -0700

    Add files via upload

--- src/Components/Employee/Requests/EmployeeRequests.js
+++ src/Components/Employee/Requests/EmployeeRequests.js
@@ -18,11 +18,22 @@ import EditIcon from "@mui/icons-material/Edit";
 import { FcSettings } from "react-icons/fc";
 import { FcFullTrash } from "react-icons/fc";
 import { FcEditImage } from "react-icons/fc";
+import { FcOk } from "react-icons/fc";
 import AddIcon from "@mui/icons-material/Add";
-import {Box, Button, Modal} from "@mui/material";
+import {
+  Box,
+  Button,
+  Modal,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+} from "@mui/material";
 import AddEmployee from "../../Admin/Employees/AddEmployee";
 import AddTimeOffRequest from "./AddTimeOffRequest";
 import AddAvailabilityRequest from "./AddAvailabilityRequest";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import notificationSound from "../../../Utils/notification.wav";
 
 const timeOffTableAttributes = [
   {
@@ -71,12 +82,12 @@ const availabilityRequestsTableAttributes = [
 ];
 
 const modalStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
+  bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
 };
@@ -88,8 +99,12 @@ const RequestsTable = ({ user }) => {
   const [error, setError] = useState(null);
   const [anchorEl, setAnchorEl] = useState(null);
   const [selectedRequest, setselectedRequest] = useState(null);
+  const [editedRequest, setEditedRequest] = useState({});
   const [addTimeOffOpen, setAddTimeOffOpen] = useState(false);
   const [addAvailabilityOpen, setAddAvailabilityOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [liveEditing, setLiveEditing] = useState(false);
+  const [removeOpen, setRemoveOpen] = useState(false);
 
   useEffect(() => {
     const fetchTimeOff = async () => {
@@ -122,23 +137,52 @@ const RequestsTable = ({ user }) => {
 
     fetchTimeOff();
     fetchAvailability();
-  }, [addTimeOffOpen, addAvailabilityOpen]);
+  }, [addTimeOffOpen, addAvailabilityOpen, isEditing]);
+
+  function formatTime(time) {
+    const utcDate = new Date(time);
+    const localDate = new Date(
+      utcDate.getTime() - utcDate.getTimezoneOffset() * 60000
+    );
+    const formattedTime = localDate
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " ")
+      .trim();
+
+    return formattedTime;
+  }
 
   const handleOpenAddTimeOff = () => {
     setAddTimeOffOpen(true);
-  }
+  };
 
   const handleCloseAddTimeOff = () => {
     setAddTimeOffOpen(false);
-  }
+  };
 
   const handleOpenAddAvailability = () => {
     setAddAvailabilityOpen(true);
-  }
+  };
 
   const handleCloseAddAvailability = () => {
     setAddAvailabilityOpen(false);
-  }
+  };
+
+  const handleOpenRemoveDialog = () => {
+    playNotificationSound();
+    handleClosePopover();
+    setRemoveOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setRemoveOpen(false);
+  };
+
+  const playNotificationSound = () => {
+    const sound = new Audio(notificationSound);
+    sound.play();
+  };
 
   const handleGearClick = (event, request) => {
     setAnchorEl(event.currentTarget);
@@ -150,13 +194,90 @@ const RequestsTable = ({ user }) => {
   };
 
   const handleEdit = () => {
+    setEditedRequest(selectedRequest);
+    setIsEditing(true);
+    setLiveEditing(true);
     handleClosePopover();
-    // Add your approve logic here
   };
 
-  const handleRemove = () => {
+  const handleEditChange = (e) => {
+    const { name, value } = e.target;
+    setEditedRequest((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSave = async () => {
+    if (!editedRequest) return;
+
+    if (!editedRequest.day_of_week) {
+      try {
+        const api = new API();
+        editedRequest.start_time = formatTime(editedRequest.start_time);
+        editedRequest.end_time = formatTime(editedRequest.end_time);
+        console.log("edited request:", editedRequest);
+        await api.updateTimeoff(
+          editedRequest,
+          formatTime(selectedRequest.start_time)
+        );
+        setIsEditing(false);
+      } catch (error) {
+        console.error("Error editing timeoff request:", error);
+      }
+    } else {
+      try {
+        const api = new API();
+        await api.updateAvailabilityRequest(editedRequest);
+        setIsEditing(false);
+      } catch (error) {
+        console.error("Error editing availability request:", error);
+      }
+    }
+
+    setIsEditing(false);
+    setLiveEditing(false);
     handleClosePopover();
-    // Add your deny logic here
+  };
+
+  const handleRemove = async () => {
+    handleCloseDialog();
+
+    if (!selectedRequest) return;
+
+    setIsEditing(true);
+
+    if (!selectedRequest.day_of_week) {
+      try {
+        const api = new API();
+        selectedRequest.start_time = formatTime(selectedRequest.start_time);
+        selectedRequest.end_time = formatTime(selectedRequest.end_time);
+        console.log("selected request:", selectedRequest);
+        await api.removeTimeoffRequest(
+          selectedRequest.employee_id,
+          selectedRequest.start_time,
+          selectedRequest.end_time,
+          selectedRequest.reason
+        );
+        setIsEditing(false);
+      } catch (error) {
+        console.error("Error deleting timeoff request:", error);
+      }
+    } else {
+      try {
+        const api = new API();
+        await api.removeAvailabilityRequest(
+          selectedRequest.employee_id,
+          selectedRequest.day_of_week
+        );
+        setIsEditing(false);
+      } catch (error) {
+        console.error("Error deleting availability request:", error);
+      }
+    }
+
+    handleClosePopover();
+    setIsEditing(false);
   };
 
   const formatDate = (dateString) => {
@@ -213,14 +334,16 @@ const RequestsTable = ({ user }) => {
             >
               {getStatusText(requestObject[attr.attributeDBName])}
             </Typography>
-          ) :attr.attributeDBName
-            ? attr.attributeDBName.includes("time")
-              ? formatDate(requestObject[attr.attributeDBName])
-              : requestObject[attr.attributeDBName]
-            : null}
+          ) : attr.attributeDBName ? (
+            attr.attributeDBName.includes("time") ? (
+              formatDate(requestObject[attr.attributeDBName])
+            ) : (
+              requestObject[attr.attributeDBName]
+            )
+          ) : null}
         </TableCell>
       ))}
-      {requestObject.status === "pending" && (
+      {requestObject.status === "Pending" && (
         <TableCell align="right">
           <IconButton
             onClick={(event) => handleGearClick(event, requestObject)}
@@ -275,7 +398,9 @@ const RequestsTable = ({ user }) => {
             >
               {getStatusText(requestObject[attr.attributeDBName])}
             </Typography>
-          ) : (requestObject[attr.attributeDBName])}
+          ) : (
+            requestObject[attr.attributeDBName]
+          )}
         </TableCell>
       ))}
       {requestObject.status === "Pending" && (
@@ -304,7 +429,7 @@ const RequestsTable = ({ user }) => {
               </ListItemIcon>
               <Typography variant="inherit">Edit</Typography>
             </MenuItem>
-            <MenuItem onClick={handleRemove}>
+            <MenuItem onClick={handleOpenRemoveDialog}>
               <ListItemIcon>
                 <FcFullTrash />
               </ListItemIcon>
@@ -328,46 +453,68 @@ const RequestsTable = ({ user }) => {
     <Fragment>
       <Box flexDirection="row">
         <Button
-            variant="contained"
-            endIcon={<AddIcon />}
-            onClick={handleOpenAddTimeOff}
-            sx={{ mb: 3, mr: 2 }}
+          variant="contained"
+          endIcon={<AddIcon />}
+          onClick={handleOpenAddTimeOff}
+          sx={{ mb: 3, mr: 2 }}
         >
           Request Time Off
         </Button>
         <Button
-            variant="contained"
-            endIcon={<AddIcon />}
-            onClick={handleOpenAddAvailability}
-            sx={{ mb: 3 }}
+          variant="contained"
+          endIcon={<AddIcon />}
+          onClick={handleOpenAddAvailability}
+          sx={{ mb: 3 }}
         >
           Request Availability
         </Button>
       </Box>
       <Modal
-          open={addTimeOffOpen}
-          onClose={handleCloseAddTimeOff}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
+        open={addTimeOffOpen}
+        onClose={handleCloseAddTimeOff}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
         <Box sx={modalStyle}>
-          <AddTimeOffRequest employee_id={user.employee_id} setAddTimeOffOpen={setAddTimeOffOpen} sx={{
-
-          }}/>
+          <AddTimeOffRequest
+            employee_id={user.employee_id}
+            setAddTimeOffOpen={setAddTimeOffOpen}
+            sx={{}}
+          />
         </Box>
       </Modal>
       <Modal
-          open={addAvailabilityOpen}
-          onClose={handleCloseAddAvailability}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
+        open={addAvailabilityOpen}
+        onClose={handleCloseAddAvailability}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
         <Box sx={modalStyle}>
-          <AddAvailabilityRequest employee_id={user.employee_id} setAddAvailabilityOpen={setAddAvailabilityOpen} sx={{
-
-          }}/>
+          <AddAvailabilityRequest
+            employee_id={user.employee_id}
+            setAddAvailabilityOpen={setAddAvailabilityOpen}
+            sx={{}}
+          />
         </Box>
       </Modal>
+      <Dialog
+        open={removeOpen}
+        onClose={handleCloseDialog}
+        aria-labelledby="confirmation-dialog-title"
+        aria-describedby="confirmation-dialog-description"
+      >
+        <DialogTitle id="confirmation-dialog-title">
+          Are you sure you want to remove this request?
+        </DialogTitle>
+        <DialogActions>
+          <Button onClick={handleRemove} color="success">
+            Yes
+          </Button>
+          <Button onClick={handleCloseDialog} color="error">
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
       <Typography variant="h6" gutterBottom component="div">
         Time Off Requests
       </Typography>
@@ -385,9 +532,56 @@ const RequestsTable = ({ user }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {timeOffRequests.map((request, idx) =>
-              renderTimeOffTableRow(request, idx)
-            )}
+            {timeOffRequests.map((request, idx) => (
+              <Fragment key={idx}>
+                {liveEditing &&
+                !selectedRequest.day_of_week &&
+                request.employee_id === selectedRequest.employee_id &&
+                request.start_time === selectedRequest.start_time &&
+                request.end_time === selectedRequest.end_time &&
+                request.status === selectedRequest.status ? (
+                  <TableRow key={idx}>
+                    {timeOffTableAttributes.map((attr, attrIdx) =>
+                      attr.attributeDBName !== "name" &&
+                      attr.attributeDBName !== "status" ? (
+                        <TableCell key={attrIdx} align={attr.align}>
+                          <TextField
+                            id={attr.attributeDBName}
+                            type={
+                              attr.attributeDBName === "start_time" ||
+                              attr.attributeDBName === "end_time"
+                                ? "datetime-local"
+                                : "text"
+                            }
+                            name={attr.attributeDBName}
+                            value={
+                              attr.attributeDBName === "start_time"
+                                ? formatTime(editedRequest.start_time)
+                                : attr.attributeDBName === "end_time"
+                                ? formatTime(editedRequest.end_time)
+                                : editedRequest[attr.attributeDBName] || ""
+                            }
+                            onChange={handleEditChange}
+                            sx={{ width: "100%" }}
+                          />
+                        </TableCell>
+                      ) : (
+                        <TableCell key={attrIdx} align={attr.align}>
+                          {request[attr.attributeDBName]}
+                        </TableCell>
+                      )
+                    )}
+                    <TableCell align="right">
+                      <IconButton onClick={() => handleSave()}>
+                        <FcOk />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ) : (
+                  renderTimeOffTableRow(request, idx)
+                )}
+              </Fragment>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
@@ -410,9 +604,71 @@ const RequestsTable = ({ user }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {availabilityRequests.map((request, idx) =>
-              renderAvailabilityTableRow(request, idx)
-            )}
+            {availabilityRequests.map((request, idx) => (
+              <Fragment key={idx}>
+                {liveEditing &&
+                request.day_of_week === selectedRequest.day_of_week &&
+                request.employee_id === selectedRequest.employee_id &&
+                request.start_time === selectedRequest.start_time &&
+                request.end_time === selectedRequest.end_time &&
+                request.status === selectedRequest.status ? (
+                  <TableRow key={idx}>
+                    {availabilityRequestsTableAttributes.map((attr, attrIdx) =>
+                      attr.attributeDBName !== "name" &&
+                      attr.attributeDBName !== "status" ? (
+                        <TableCell key={attrIdx} align={attr.align}>
+                          {attr.attributeDBName === "day_of_week" ? (
+                            <Select
+                              value={editedRequest.day_of_week || ""}
+                              onChange={(e) =>
+                                handleEditChange(e.target.value, "day_of_week")
+                              }
+                              sx={{ width: "100%" }}
+                            >
+                              <MenuItem value={"Sunday"}>Sunday</MenuItem>
+                              <MenuItem value={"Monday"}>Monday</MenuItem>
+                              <MenuItem value={"Tuesday"}>Tuesday</MenuItem>
+                              <MenuItem value={"Wednesday"}>Wednesday</MenuItem>
+                              <MenuItem value={"Thursday"}>Thursday</MenuItem>
+                              <MenuItem value={"Friday"}>Friday</MenuItem>
+                              <MenuItem value={"Saturday"}>Saturday</MenuItem>
+                            </Select>
+                          ) : (
+                            <TextField
+                              id={attr.attributeDBName}
+                              type={
+                                attr.attributeDBName === "start_time" ||
+                                attr.attributeDBName === "end_time"
+                                  ? "time"
+                                  : "text"
+                              }
+                              name={attr.attributeDBName}
+                              value={editedRequest[attr.attributeDBName] || ""}
+                              onChange={handleEditChange}
+                              InputLabelProps={{
+                                shrink: true, // Ensures that the label is always displayed
+                              }}
+                              sx={{ width: "100%" }}
+                            />
+                          )}
+                        </TableCell>
+                      ) : (
+                        <TableCell key={attrIdx} align={attr.align}>
+                          {request[attr.attributeDBName]}
+                        </TableCell>
+                      )
+                    )}
+                    <TableCell align="right">
+                      <IconButton onClick={() => handleSave()}>
+                        <FcOk />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ) : (
+                  renderAvailabilityTableRow(request, idx)
+                )}
+              </Fragment>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
